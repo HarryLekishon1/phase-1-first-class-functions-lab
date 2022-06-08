052b1d31180be0af18fa2a330889b09705fd2ed6
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-08 14:36:43 +0300

    commit tree ac42556a27ed35c9c6ff02b2420390b8a54112da

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const newDrivers = drivers.slice(0,2);
+
+return newDrivers;
+}
+
+function returnLastTwoDrivers(arrayOfDrivers){
+    
+    const newDrivers = arrayOfDrivers.slice(arrayOfDrivers.length-2);
+    return newDrivers;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(){
+    return function fareMultiplier(num){
+        return num*num;
+    };
+}
+
+function fareDoubler(fare){
+
+    return fare*2;
+}
+function fareTripler(fare) {
+    return fare *3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers){
+
+    return returnLastTwoDrivers(arrayOfDrivers);
+}
+
+
+    
